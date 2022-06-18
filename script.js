b1d747f7966e259af8b2f79ba4e6ab4606a41462
sankalp142002 <sankalp121314@gmail.com>
2022-06-18 08:58:12 +0530

    second commit

--- script.js
+++ script.js
@@ -1,37 +1,76 @@
 const questionData = [
     {
-        question: 'what5 is your name?',
-        a: 'faw',
-        b: 'uikbgfvc',
-        c: 'hjVcu',
-        d: '1',
+        question: 'Do you Play laser tag once a week?',
+        a: 'Yes',
+        b: 'NO',
+        c: 'Yeno',
+        d: 'I am not awsome!',
         correct : 'a'
     },
     {
-        question: 'what is your age?',
-        a: 'faw',
-        b: 'uikbgfvc',
-        c: 'hjVcu',
-        d: '3',
-        correct : 'b'
-    },
-    {
-        question: 'what is your mothers name?',
-        a: 'faw',
-        b: 'uikbgfvc',
-        c: 'hjVcu',
-        d: '4',
+        question: "Don't get married before you're ______ ?",
+        a: '15',
+        b: '25',
+        c: '30',
+        d: '69',
         correct : 'c'
     },
     {
-        question: 'hjyfu est 8iynp rt?',
-        a: 'faw',
-        b: 'uikbgfvc',
-        c: 'hjVcu',
-        d: '2',
+        question: 'Keep your apartment chilly. Nipples reveal themselves at temperatures below ______°C.',
+        a: '10',
+        b: '15',
+        c: '-1',
+        d: '33',
+        correct : 'b'
+    },
+    {
+        question: 'Do you Have a “guy” for everything?',
+        a: 'Yes',
+        b: 'NO',
+        c: 'Yeno',
+        d: 'I am not awsome!',
+        correct : 'a'
+    },
+    {
+        question: 'Never use the word “______” on a first date.',
+        a: 'Hey!',
+        b: 'Handcuff',
+        c: 'Wet',
+        d: 'moist',
         correct : 'd'
     },
-
+    {
+        question: 'Will learning to play the air drums save your life one day?',
+        a: 'Yes',
+        b: 'NO',
+        c: 'Yeno',
+        d: 'I am not awsome!',
+        correct : 'a'
+    },
+    {
+        question: 'When visiting a strip club, do not sleep on the meatballs. True or False?',
+        a: 'False',
+        b: 'True',
+        c: 'Not True',
+        d: 'I am not awsome!',
+        correct : 'b'
+    },
+    {
+        question: 'The words said by gods ______.',
+        a: 'Never ever get too drunk!!!',
+        b: 'Marry a striper',
+        c: 'Slender wife, happy life!!!',
+        d: 'Dog is the best wingman ever!',
+        correct : 'c'
+    },
+    {
+        question: 'Own at least one suit, but twelve if you can.',
+        a: 'Yes',
+        b: 'A hell yesss!!!',
+        c: 'A legen yessssss dary!!!',
+        d: 'I am not awsome at all bro!!',
+        correct : 'c'
+    },
 ]
 
 
